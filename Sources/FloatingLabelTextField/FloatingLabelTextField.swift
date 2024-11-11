4f11daa877d8e833f98b353d3a31ba8d981aa1c5
ChhanSophearath <130240094+CSR-Rath@users.noreply.github.com>
2024-11-11 14:04:21 +0700

    update func

--- Sources/FloatingLabelTextField/FloatingLabelTextField.swift
+++ Sources/FloatingLabelTextField/FloatingLabelTextField.swift
@@ -301,23 +301,23 @@ extension FloatingLabelTextField {
 }
 
 // MARK: - Handle Array FloatingLabelTextField
-extension UIView {
-    
-    public func arrayTextFieldsResignFirstResponder(textFields: [FloatingLabelTextField]) {
-        textFields.forEach { $0.resignFirstResponder() }
-    }
-    
-    public func isValidateTextField(textFields: [FloatingLabelTextField],
-                                    success: @escaping (_ success: FloatingLabelTextField) -> (),
-                                    failure: @escaping (_ failure: FloatingLabelTextField) -> ()) {
-        textFields.forEach { item in
-            if (item.text == "" || item.text == "  ") && !item.isOptionalField {
-                item.isValidate = false
-                failure(item)
-            } else {
-                success(item)
-                item.isValidate = true
-            }
-        }
-    }
-}
+//extension UIView {
+//    
+//    public func arrayTextFieldsResignFirstResponder(textFields: [FloatingLabelTextField]) {
+//        textFields.forEach { $0.resignFirstResponder() }
+//    }
+//    
+//    public func isValidateTextField(textFields: [FloatingLabelTextField],
+//                                    success: @escaping (_ success: FloatingLabelTextField) -> (),
+//                                    failure: @escaping (_ failure: FloatingLabelTextField) -> ()) {
+//        textFields.forEach { item in
+//            if (item.text == "" || item.text == "  ") && !item.isOptionalField {
+//                item.isValidate = false
+//                failure(item)
+//            } else {
+//                success(item)
+//                item.isValidate = true
+//            }
+//        }
+//    }
+//}
